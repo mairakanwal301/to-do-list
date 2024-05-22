0c350a4a9ad949133790de9d84d5adffa0a0ed0e
mairarafiq301 <mairarafiq301@gmail.com>
2024-05-22 04:39:06 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "addtodo",
+            type: "input",
+            message: "What you want to add in your todos?",
+        },
+        {
+            name: "addmoretodos",
+            type: "confirm",
+            message: "Do you want to add more todos?",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.addtodo);
+    condition = addTask.addmoretodos;
+    console.log(todos);
+}
